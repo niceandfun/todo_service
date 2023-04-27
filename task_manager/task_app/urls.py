a07ea37ac9e99e64cfc68ec251bfa9fd1494eef2
nikita.druzhinin <nikita.druzhinin@myoffice.team>
2023-04-27 22:48:41 +0300

    Signup and login is done

--- task_manager/task_app/urls.py
+++ task_manager/task_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.TasksListView.as_view(), name='task_list'),
-    path('tasks/<int:pk>', views.TaskDetailView.as_view(), name='task_detail')
+    path("", views.home_page, name="home_page"),
+    path('task_list/', views.TasksListView.as_view(), name='task_list'),
+    path('task_list/<int:pk>', views.TaskDetailView.as_view(), name='task_detail'),
+    path("signup/", views.SignUp.as_view(), name="signup"),
 ]
--- task_manager/task_app/views.py
+++ task_manager/task_app/views.py
@@ -1,11 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from django.views import generic
 
-from task_app.models import Task
+from .models import Task
+from django.shortcuts import render
+
+
+def home_page(request):
+    return render(request, "task_app/index.html")
 
 
 class TasksListView(generic.ListView):
     model = Task
-    template_name = 'task_app/index.html'
+    template_name = 'task_app/task_list.html'
     context_object_name = 'task_list'
 
 
@@ -13,3 +20,9 @@ class TaskDetailView(generic.DetailView):
     model = Task
     template_name = 'task_app/task_detail.html'
     context_object_name = 'task'
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
