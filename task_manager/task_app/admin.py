50ecea20971af1c6ab43dc009ff9a9e68ea3da26
nikita.druzhinin <nikita.druzhinin@myoffice.team>
2023-04-27 18:40:14 +0300

    Registered models

--- task_manager/task_app/admin.py
+++ task_manager/task_app/admin.py
@@ -1,3 +1,19 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, TaskStatus, Author
+
+
+@admin.register(Task)
+class Task(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class Task(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskStatus)
+class Task(admin.ModelAdmin):
+    pass
