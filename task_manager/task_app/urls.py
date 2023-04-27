ec451dbd8c9c7094b183a19112e65b83c44f0bfe
nikita.druzhinin <nikita.druzhinin@myoffice.team>
2023-04-27 18:25:22 +0300

    fixed url paths

--- task_manager/task_app/urls.py
+++ task_manager/task_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.TasksListView.as_view(), name='task_list'),
-    path('tasks/<pk:int>', views.TaskDetailView.as_view(), name='task_detail')
+    path('tasks/<int:pk>', views.TaskDetailView.as_view(), name='task_detail')
 ]
